71e1fd203fc093dbce854f30682458377ac8d038
mandyhtsai <htsai2@uci.edu>
2021-01-23 15:14:56 -0800

    added 6 unit tests

--- src/test/java/org/json/junit/XMLM2Test.java
+++ src/test/java/org/json/junit/XMLM2Test.java
@@ -1,23 +1,67 @@
+/**
+ * @author Mandy Tsai
+ * 1/20/2021
+ */
+
 package org.json.junit;
+
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.nio.file.Paths;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.json.JSONPointer;
 import org.json.XML;
+import org.junit.Test;
 
 public class XMLM2Test {
-    public static void main(String[] args) {
-        String file = "C:\\Users\\Mandy\\Desktop\\MSWE\\Winter2021\\SWE262P\\JSON-java\\samples\\enwiki-20201220-abstract19.xml";
-        JSONPointer path = new JSONPointer("/feed/doc");
-        try {
-            long start = System.nanoTime();
-            XML.toJSONObject(new FileReader(new File(file)), path);
-            System.out.println(System.nanoTime() - start);
-        }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+    private final static String DIR = Paths.get("samples").toAbsolutePath().toString();
+
+    @Test
+    public void toJSONObjectStringsXMLExtractEmptyPath() throws FileNotFoundException {
+        JSONPointer path = new JSONPointer("/");
+        JSONObject o = XML.toJSONObject(new FileReader(new File(DIR, "strings.xml")), path);
+        assertTrue(o instanceof JSONObject);
+        assertTrue(o.length() == 0);
+    }
+
+    @Test
+    public void toJSONObjectStringsXMLExtractRoot() throws FileNotFoundException {
+        JSONPointer path = new JSONPointer("/resources");
+        JSONObject o = XML.toJSONObject(new FileReader(new File(DIR, "strings.xml")), path);
+        assertTrue(o.get("resources") instanceof JSONObject);
+    }
+
+    @Test
+    public void toJSONObjectStringsXMLExtractNestedKey() throws FileNotFoundException {
+        JSONPointer path = new JSONPointer("/resources/string-array");
+        JSONObject o = XML.toJSONObject(new FileReader(new File(DIR, "strings.xml")), path);
+        assertTrue(((JSONArray) o.get("string-array")).length() == 2);
+    }
+
+    @Test
+    public void toJSONObjectBooksXMLExtractRoot() throws FileNotFoundException {
+        JSONPointer path = new JSONPointer("/catalog");
+        JSONObject o = XML.toJSONObject(new FileReader(new File(DIR, "books.xml")), path);
+        assertTrue(o.get("catalog") instanceof JSONObject);
+    }
+
+    @Test
+    public void toJSONObjectBooksXMLExtractNestedKey() throws FileNotFoundException {
+        JSONPointer path = new JSONPointer("/catalog/book");
+        JSONObject o = XML.toJSONObject(new FileReader(new File(DIR, "books.xml")), path);
+        assertTrue(((JSONArray) o.get("book")).length() == 12);
+    }
+
+    @Test
+    public void toJSONObjectTestXMLExtractNestedKey() throws FileNotFoundException {
+        JSONPointer path = new JSONPointer("/nested-array");
+        JSONObject o = XML.toJSONObject(new FileReader(new File(DIR, "test.xml")), path);
+        assertTrue(o.get("nested-array") instanceof JSONObject);
+        assertTrue(((JSONObject) o.get("nested-array")).get("ch") instanceof JSONObject);
     }
 }
